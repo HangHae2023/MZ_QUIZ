9688632ba269e4808322bd2452e69b2392e5cca9
rodinkim <124944568+rodinkim@users.noreply.github.com>
2023-03-23 19:11:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const logger = require('./config/logger')
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
     optionsSuccessStatus: 200,
     exposedHeaders: ['Authorization'],
